aeb176569002ed0cd36b2a7373ad1b64004b94ac
Junayed5 <mdjunayed352003@gmail.com>
2025-04-30 23:13:08 +0600

    first commit

---
+++ module1/dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+let course = "I am started next level typescript";
+console.log(course);
---
+++ module1/src/index.ts
@@ -0,0 +1,2 @@
+let course: string = "I am started next level typescript";
+console.log(course);
