fd9b812854f1b9bb5588a761062eb6d9e79ff0b2
Junayed5 <mdjunayed352003@gmail.com>
2025-05-01 14:25:06 +0600

    all type

---
+++ module1/src/1.4.ts
@@ -0,0 +1,33 @@
+//Basic Data Types
+
+// Primitive Data Types//
+
+//string
+let myName: string = "John Doe";
+
+//number
+let roll: number = 123;
+
+//boolean
+let isActive: boolean = true;
+
+//undefined
+let x: undefined = undefined;
+
+//null
+let y: null = null;
+
+let d: number;
+d= 134;
+
+// Non-Primitive Data Types//
+
+//Array
+
+let friends: string[] = ["sifat", "Shariat", "Hossain"];
+
+let rollNumber: number[] = [125, 126]
+
+// Tuple--> array--> order --> type of values
+
+let student: [string, number, boolean] = ["sifat", 123, true];
